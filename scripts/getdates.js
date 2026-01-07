8102914d39dc6b8aa19cb3a6abd9728562b45208
arebaza <arebaza@byupathway.edu>
2026-01-07 10:19:06 -0700

    F-1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,7 @@
 // Footer dates required by WDD 131 rubric
-const spanAnioActual = document.querySelector("#currentyear");
-spanAnioActual.textContent = new Date().getFullYear();
 
-const spanUltimaModificacion = document.querySelector("#lastmodified");
-spanUltimaModificacion.textContent = document.lastModified;
+const yearSpan = document.querySelector("#currentyear");
+yearSpan.textContent = new Date().getFullYear();
+
+const modifiedSpan = document.querySelector("#lastmodified");
+modifiedSpan.textContent = document.lastModified;
